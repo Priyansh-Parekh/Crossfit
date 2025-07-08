91f4284c5a53d8f736745bf9201a99813d1d42c9
jake-thomas1 <mathewjake733@gmail.com>
2025-07-08 21:04:08 +0530

    updated login

--- server/route/auth.js
+++ server/route/auth.js
@@ -9,3 +9,31 @@ route.get('/register', (req, res) => {
 });
 
 //Post Register Page
+route.post('/register', async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const UserExists = await User.findOne({ email});
+  if(UserExists) [
+    return res.render('auth/register', { error: 'User already exists'})
+  ];
+
+  const hashpassword = await bcrypt.hash(password, 10);
+  const newUser = new User({ name, email, password: hashpassword});
+  await.newUser.save();
+  res.redirect('/auth/login');
+});
+
+//GET login page
+
+route.get('/login', (req, res) => {
+  res.render('auth/login', {error: null});
+})
+
+//POST login page
+route.post('/login', async (req, res) => {
+  const {email, password} = req.body;
+  const user = await User.findOne({ email });
+  if(!user) {
+    return res.render('auth/login', { error: 'Invalid email or password'});
+    }
+  }
