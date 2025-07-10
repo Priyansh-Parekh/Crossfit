34b1dfa09d16bd1740790add2abbac14b6562509
jake-thomas1 <mathewjake733@gmail.com>
2025-07-11 03:39:11 +0530

    small progress in merchandise, i need the mongoDB link to proceed further with this since the merch should be avialable in the mongoDB( right now its empty ). and once we put the merch in the database we can filter it

--- server/route/merchandise.js
+++ server/route/merchandise.js
@@ -4,14 +4,22 @@ const route = express.Router();
 const axios = require('axios');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+//fetching data
+const merchandise = require("../models/merchandise");
 
 
 
-//Home Page
-route.get('/', async (req, res) => {
-    res.render("merchandise");
-})
+route.get('/merchandise', async (req, res) => {
+  try {
+    const allMerchandise = await merchandise.find(); // fetch all merchandise
+    res.render('merchandise', { merchandise: allMerchandise }); // ✅ pass to EJS
+  } catch (error) {
+    console.error("Error fetching merchandise:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
+module.exports = route;
 
 
 
